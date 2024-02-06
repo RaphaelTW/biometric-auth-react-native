48102c0779f9b62a7cc2d9cdbd6d742c7411200a
Raphael Laurentino da Silva <77465404+RaphaelTW@users.noreply.github.com>
2024-02-06 17:15:39 -0300

    up

--- App.tsx
+++ App.tsx
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import * as LocalAuthentication from 'expo-local-authentication';
+import { styles } from './styles.ts';
 
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  async function verifyAvaiableAuthentication(){
+    const compatible = await LocalAuthentication.hasHardwareAsync();
+    console.log(compatible);
+
+    const types = await LocalAuthentication.supportedAuthenticationTypesAsync();
+    console.log(types.map(type => LocalAuthentication.AuthenticationType[type]));
+  }
+
+  async function handleAuthentication(){
+    const isBiometricEnrolled = await LocalAuthentication.isEnrolledAsync();
+    console.log(isBiometricEnrolled);
+
+    if(!isBiometricEnrolled){
+      return Alert.alert('Alerta', 'Nenhuma biometria encontrada. Cadastre no dispositivo!');
+    }
+
+    const auth = await LocalAuthentication.authenticateAsync({
+      promptMessage: 'Login com Biometria',
+      fallbackLabel: 'Biometria não reconhecia'
+    });
+
+    setIsAuthenticated(auth.success);
+
+  }
+
+  useEffect(() => {
+    verifyAvaiableAuthentication();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Usuário conectado: { isAuthenticated ? 'Sim' : 'Não' }</Text>
+
+      <Button title="Entrar" onPress={handleAuthentication}/>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ README.md
@@ -0,0 +1,44 @@
+# Autenticação Biométrica com React Native
+
+Este aplicativo React Native permite aos usuários autenticar-se usando autenticação biométrica (como impressão digital ou reconhecimento facial).
+
+## Funcionalidades
+
+- Verificação da disponibilidade de autenticação biométrica no dispositivo.
+- Autenticação do usuário usando biometria.
+- Exibição do status de autenticação do usuário.
+
+## Como usar
+
+### Pré-requisitos
+
+Certifique-se de ter o ambiente de desenvolvimento React Native configurado no seu sistema. Você também precisará de um dispositivo físico com suporte à autenticação biométrica ou um emulador configurado.
+
+### Instalação
+
+1. Clone este repositório:
+
+   ```bash
+   git clone https://github.com/RaphaelTW/biometric-auth-react-native.git
+
+
+2. Navegue até o diretório do projeto:
+cd biometric-auth-react-native
+
+
+3. Instale as dependências:
+npm install
+
+### Execução
+
+4. Execute o aplicativo no seu dispositivo ou emulador:
+npm start
+
+### Nota
+Certifique-se de ter configurado corretamente o ambiente para desenvolvimento React Native, incluindo as variáveis de ambiente necessárias e as ferramentas necessárias instaladas.
+
+### Contribuição
+Contribuições são bem-vindas! Sinta-se à vontade para enviar pull requests ou abrir issues relatando problemas ou sugestões de melhoria.
+
+### Licença
+Este projeto está licenciado sob a Licença MIT.
